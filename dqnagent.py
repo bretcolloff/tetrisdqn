1fa0134bad4558ce2fae65c373050c604e74f64a
Bret Colloff <bret.colloff@gmail.com>
2018-12-09 12:14:32 +0000

    Change model to CNN?

--- dqnagent.py
+++ dqnagent.py
@@ -3,7 +3,7 @@ import random
 from tetrisgym import Move, TetrisGym
 from collections import deque
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, MaxPooling2D, Conv2D, Flatten
 from keras.optimizers import Adam
 
 import matplotlib.pyplot as plt
@@ -28,13 +28,27 @@ class DQN:
 
     def _build_model(self):
         # Neural Net for Deep-Q learning Model
+        #model = Sequential()
+        #model.add(Dense(30, input_dim=self.state_size, activation='relu'))
+        #model.add(Dense(30, activation='relu'))
+        #model.add(Dense(30, activation='relu'))
+        #model.add(Dense(self.action_size, activation='linear'))
+        #model.compile(loss='mse',
+        #              optimizer=Adam(lr=self.learning_rate))
+
         model = Sequential()
-        model.add(Dense(30, input_dim=self.state_size, activation='relu'))
-        model.add(Dense(30, activation='relu'))
-        model.add(Dense(30, activation='relu'))
-        model.add(Dense(self.action_size, activation='linear'))
-        model.compile(loss='mse',
-                      optimizer=Adam(lr=self.learning_rate))
+        model.add(Conv2D(32, kernel_size=(3, 3), strides=(1, 1),
+                         activation='relu',
+                         input_shape=(20, 10, 1)))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        model.add(Conv2D(64, (3, 3), activation='relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Flatten())
+        model.add(Dense(1000, activation='relu'))
+        model.add(Dense(self.action_size, activation='softmax'))
+        adam = Adam(lr=1e-6)
+        model.compile(loss='mse', optimizer=adam)
+
         return model
 
     def remember(self, state, action, reward, next_state, done):
@@ -43,7 +57,7 @@ class DQN:
     def act(self, state):
         if np.random.rand() <= self.epsilon:
             return random.randrange(self.action_size)
-        act_values = self.model.predict(state.reshape(-1, 200))
+        act_values = self.model.predict(state.reshape((1, 20, 10, 1)))
         return np.argmax(act_values[0])
 
     def integer_to_action(self, input):
@@ -59,15 +73,15 @@ class DQN:
     def replay(self, batch_size):
         minibatch = random.sample(self.memory, batch_size)
         for state, action, reward, next_state, done in minibatch:
-            next_state = next_state.reshape(-1, 200)
-            state = state.reshape(-1, 200)
+            next_state = next_state
+            state = state
             target = reward
             if not done:
                 target = (reward + self.gamma *
-                          np.amax(self.model.predict(next_state)[0]))
-            target_f = self.model.predict(state)
+                          np.amax(self.model.predict(next_state.reshape((1, 20, 10, 1)))[0]))
+            target_f = self.model.predict(state.reshape((1, 20, 10, 1)))
             target_f[0][int(action)] = target
-            self.model.fit(state, target_f, epochs=1, verbose=0)
+            self.model.fit(state.reshape((1, 20, 10, 1)), target_f, epochs=1, verbose=0)
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
@@ -100,8 +114,8 @@ for episode in range(2000):
         else:
             for state, action, reward, next_state in update:
                 reward = reward
-                next_state = next_state.reshape(-1)
-                state = state.reshape(-1)
+                next_state = next_state
+                state = state
                 agent.remember(state, action, reward, next_state, False)
                 state = next_state
         if done:
